3a2be1207b49d314665df3ae8a563b668ab7186b
Nikolay Kamyshnikov <mykola.kamyshnikov@gmail.com>
2023-02-15 14:05:07 +0200

    build body project

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 
 export default defineConfig({
   root: 'src',
   build: {
+    rollupOptions: {
+      input: {
+      main: resolve (__dirname, 'src/index.html'),
+      second: resolve (__dirname, 'src/restaurant.html'),
+      next: resolve (__dirname, 'src/hotel.html'),
+      next: resolve (__dirname, 'src/room.html')
+      },
+    },
     outDir: '../dist',
   },
   plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
