4c79e55d15e9d433d7fa911fa09294d40843c35d
vadimfront <vadimzila94@gmail.com>
2023-02-20 00:21:29 +0200

    modal test

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,11 +1,16 @@
+import $ from 'jquery';
+window.$ = $;
+
 $(function () {
   function modal() {
-    $('.open_modal').on('click', function () {
+    $('.open_modal').on('click', function (e) {
+      e.preventDefault();
       console.log('open_modal');
       const modal_id = $(this).attr('data-modal-id');
       $(modal_id).addClass('active_modal');
     });
     $('.modal, .modal_close').on('click', function (e) {
+      e.preventDefault();
       const el = e.target;
       if (
         el.classList.contains('modal') ||
