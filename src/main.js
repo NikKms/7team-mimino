96b9a5125b738bf91e0e17be28f924e203964c87
Nikolay Kamyshnikov <mykola.kamyshnikov@gmail.com>
2023-02-21 12:15:11 +0200

    7777777

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-// import './js/jquery-3.6.3'
+import '/js/jquery-3.6.3'
 
-// import './js/swiper'
-// import './js/slick'
-// import './js/slider'
-// import './js/modal'
 
-// import './js/jquery.mask'
+import '/js/slick'
+import '/js/slider'
+import '/js/modal'
+
+import '/js/jquery.mask'
+
+import '/js/swiper'
+
