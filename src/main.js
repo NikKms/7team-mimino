aaf1b9881239e7396a06926bde47bfc878e24e70
Nikolay Kamyshnikov <mykola.kamyshnikov@gmail.com>
2023-02-21 11:07:40 +0200

    test 4443

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
-import './js/swiper';
-import './js/slick';
-import './js/slider';
-import './js/modal';
+// import './js/jquery-3.6.3'
+
+// import './js/swiper'
+// import './js/slick'
+// import './js/slider'
+// import './js/modal'
+
+// import './js/jquery.mask'
