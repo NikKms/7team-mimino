c63e17111940d5aee9d2af10ea933099ef3ac8a2
NikKms <119286614+NikKms@users.noreply.github.com>
2023-02-22 10:12:59 +0000

    Deploying to gh-pages from @ NikKms/7team-mimino@26f0d3ff27e14d396f539152eb91f4c656b918d3 🚀

--- assets/room-bcc58e76.js
+++
@@ -1,1 +0,0 @@
-import"./modal-71c8453d.js";new Swiper(".swiper",{direction:"horizontal",loop:!0,speed:1e3,parallax:!0,spaceBetween:30,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
